5dc6bae52a862801252b6b8abf65f3281a4f2187
tarek <sitarek77@gmail.com>
2024-09-26 09:55:20 +0600

    morning

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:'https://world.openfoodfacts.org/api/v3/product/',
+  baseURL:'https://world.openfoodfacts.org/api/v3/product/737628064502.json',
   params:{
-     key:'737628064502.json'
+    key:'106ef8976897e41f30f4b29daa12481a',
+    token:'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMDZlZjg5NzY4OTdlNDFmMzBmNGIyOWRhYTEyNDgxYSIsIm5iZiI6MTcyNzI4OTIzNC42OTU1MTksInN1YiI6IjY2ZjQ1NWE0MGVjYTE3ZGExYjBlMDdlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JK-X-PTzUOEMLiQVbWMjV5FwjOKMZnP6n2Iu0vkrf20'
   }
 })
